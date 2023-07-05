51c23ae2629fd27dfecd92c70dd393655be43f11
Moona <moonapark1126@gmail.com>
2023-07-05 20:47:13 +0900

    2.7

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-// const amIFat=null;
-// let something;
-// console.log(something);
-
- days0fWeek=['mon','tue','wed','thu','fri','sat'];
- //Get Item from Array
- console.log(days0fWeek[2]);
-
-//Add one more day to the Array
-days0fWeek.push('sun');
-console.log(days0fWeek);
+function sayHello(){
+    console.log("Hello! myname is");
+}
+sayHello(" moon");
+sayHello("apple");
+sayHello("banana");

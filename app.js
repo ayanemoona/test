d7930c7dc4dc655b317c080eb18122967189df31
Moona <moonapark1126@gmail.com>
2023-07-05 13:00:33 +0900

    appa

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// const amIFat=null;
+// let something;
+// console.log(something);
 
+ days0fWeek=['mon','tue','wed','thu','fri','sat'];
+ //Get Item from Array
+ console.log(days0fWeek[2]);
+
+//Add one more day to the Array
+days0fWeek.push('sun');
+console.log(days0fWeek);

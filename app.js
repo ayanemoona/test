9e09e9a5d9618dd4317dd6eb0709c79d8433017f
Moona <moonapark1126@gmail.com>
2023-07-07 20:42:54 +0900

    7/7

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-function sayHello(){
-    console.log("Hello! myname is");
-}
-sayHello(" moon");
-sayHello("apple");
-sayHello("banana");
+const calculator={
+    add:function(a,b){
+        console.log(a+b);
+    },
+    minus:function(a,b){
+        console.log(a-b);
+    },
+    division:function(a,b){
+        console.log(a/b);    
+    },
+    square:function(a,b){
+        console.log(a**b);
+    }
+    
+};
+calculator.add(5,1);
+calculator.minus(5,1);
+calculator.division(5,1);
+calculator.square(5,1);

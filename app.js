8f3000a087ef72633bfa30ededbf479e738f95a3
Moona <moonapark1126@gmail.com>
2023-07-10 21:40:51 +0900

    12345678

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const calculator={
-    add:function(a,b){
-        console.log(a+b);
-    },
-    minus:function(a,b){
-        console.log(a-b);
-    },
-    division:function(a,b){
-        console.log(a/b);    
-    },
-    square:function(a,b){
-        console.log(a**b);
-    }
-    
-};
-calculator.add(5,1);
-calculator.minus(5,1);
-calculator.division(5,1);
-calculator.square(5,1);
+const age =parseInt(prompt("how old are you?"));
+
+console.log(typeof age);

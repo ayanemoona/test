d0f55ab63594a373c1409ccd1a2706756ea7cd74
Moona <moonapark1126@gmail.com>
2023-07-12 12:22:22 +0900

    7/12

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const age =parseInt(prompt("how old are you?"));
 
-console.log(isNaN(age));
 
-if (isNaN(age)){
+if (isNaN(age) || age <0){
     /// condition ===ture
-    console.log("Please write a number");
-}else{
+    console.log("Please write a real positive number");
+}else if(age<18){
     //condition===false
-    console.log("Thank you for writing your age");
+    console.log("You ate too young.");
+}else if(age>=18 &&age<=50){
+    console.log("You can drink");
+}else if (age>50 && age<=80){
+    console.log("You should exercise");
+}else if(age ==100){
+    console.log("wow you are wise");
+}else if (age>80){
+    console.log(" You can do whatever younwant.");
 }
+

6641725bcc523b01e518386efc5107972cb5d10b
Moona <moonapark1126@gmail.com>
2023-07-30 22:33:46 +0900

    moon

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-const bye = document.querySelector("div.bye:first-child h1");
-const bye2 = document.querySelector("div.bye:h2");
-function handleTitleClik(){
-    const clickedClass = "clickedClass";
-    h1.classList.toggle(clickedClass);
-  
+const loginInput=document.querySelector("#login-form input"); 
+const loginbutton=document.querySelector("#login-form button");
 
-    
+
+function onLoginClick(){
+    const username =loginInput.value;
+    console.log(username);
+
+
 }
-
-h1.addEventListener("click",handleTitleClik);
-bye.addEventListener("click",handleTitleClik);
-bye2.addEventListener("click",handleTitleClik);
+loginbutton.addEventListener("click",onLoginClick)
